b601ec977e430680457f8abdffe3f49012e6d536
Ghhyamjo Lama <107875745+TmgGems@users.noreply.github.com>
2024-06-10 10:10:59 +0545

    Add files via upload

---
+++ ThePretendCompany/ThePretendCompany/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TCPData;
+using TCPExtensions;
+namespace ThePretendCompany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employeeList = Data.GetEmployees();
+            var filteredEmployees = employeeList.Filter( emp => emp.IsManager == true );
+
+            foreach(var employee in filteredEmployees)
+            {
+                Console.WriteLine($"First Name : {employee.FirstName}");
+                Console.WriteLine($"Last Name : {employee.LastName}");
+                Console.WriteLine($"Annual Salary : {employee.AnnualSalary}");
+                Console.WriteLine($"Manager :{employee.IsManager}");
+
+            }
+        }
+    }
+}
